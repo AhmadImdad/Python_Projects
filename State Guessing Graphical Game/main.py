4f53b5d1ac0358f6cd2a7c379853da65c9c2bea4
Ahmad Imdad <131014690+AhmadImdad@users.noreply.github.com>
2024-06-28 16:50:51 +0500

    State Guessing Graphical Game
    Used pandas and turtle library to produce a graphical game to implement both library concepts

---
+++ State Guessing Graphical Game/main.py
@@ -0,0 +1,58 @@
+import turtle
+
+import pandas
+import pandas as pd
+from turtle import Turtle
+
+IMAGE = "blank_states_img.gif"
+
+screen = turtle.Screen()
+screen.setup(800, 600)
+screen.addshape(IMAGE)
+turtle.shape(IMAGE)
+
+data = pd.read_csv("50_states.csv")
+states = data["state"].tolist()
+
+for i in range(len(states)):
+    states[i] = states[i].lower()
+
+x_cord = data["x"].tolist()
+y_cord = data["y"].tolist()
+
+game_on = True
+guessed = 0
+answered_states = list()
+
+while game_on:
+    index = 0
+    state_name = turtle.textinput(f"{guessed}/50 Guessed",
+                                  "Write the state's name : ")
+    state_name = state_name.lower()
+    for names in states:
+        if state_name == names:
+            state_turtle = Turtle()
+            state_turtle.penup()
+            state_turtle.hideturtle()
+            # A turtle writes text wherever it is standing at the moment
+            state_turtle.goto(x_cord[index], y_cord[index])
+            state_turtle.write(arg=state_name, align="center",
+                               font=("Times New Roman", 10, "normal"))
+            guessed += 1
+            answered_states.append(state_name)
+            break
+        index += 1
+
+    if index == len(states):
+        game_on = False
+        temp_list = list()
+        for names in states:
+            if not (names in answered_states):
+                temp_list.append(names)
+        data_dict = {
+            "states": temp_list
+        }
+        temp_DF = pandas.DataFrame(data_dict)
+        temp_DF.to_csv("States_to_learn.csv")
+
+screen.exitonclick()
